d792ad0027e8d389185e449e6366ab6bd651d07e
Bharath Krishna <phantomkrisx@gmail.com>
2026-01-24 16:47:06 +0530

    add pyramid pattern printing functionality

---
+++ pyramid.py
@@ -0,0 +1,8 @@
+rows=4
+for i in range(1,rows+1):
+    print(" "*(rows-i),end="")
+    for j in range(1,i+1):
+        print(j,end=" ")
+    for k in range(j-1,0,-1):
+        print(k,end=" ")
+    print()
