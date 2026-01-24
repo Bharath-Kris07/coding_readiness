ba863d1722729d0c38e5753c9ee69aa8d1f21ae6
Bharath Krishna <phantomkrisx@gmail.com>
2026-01-24 16:48:48 +0530

    fix spacing in pyramid pattern output

--- pyramid.py
+++ pyramid.py
@@ -1,6 +1,6 @@
 rows=4
 for i in range(1,rows+1):
-    print(" "*(rows-i),end="")
+    print("  "*(rows-i),end="")
     for j in range(1,i+1):
         print(j,end=" ")
     for k in range(j-1,0,-1):
