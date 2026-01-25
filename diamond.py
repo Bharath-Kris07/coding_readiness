f0142256168ebb8a7544e1eccb6eab2bedcdab08
Bharath Krishna <phantomkrisx@gmail.com>
2026-01-25 15:03:13 +0530

    add comment for clarity on spacing between stars in diamond pattern

--- diamond.py
+++ diamond.py
@@ -3,7 +3,7 @@ for i in range(1,n+1):
     print(" "*(n-i),end="")
     print('*',end="")
     if i != 1:
-        print(" "*(2*i-3),end='*')
+        print(" "*(2*i-3),end='*') # for spaces between stars (1-3-5)
     print()
 for i in range(n-1,0,-1):
     print(" "*(n-i),end="")
